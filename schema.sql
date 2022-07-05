331262eb0702ec2eddcec72a751ae7ff06282a45
Chia Carlyle <chiacarlyle@gmail.com>
2022-07-05 11:28:59 +0100

    Update schema.sql
    Add a column called `species` to the animals table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE animals (
     id integer, name varchar(50), date_of_birth date, escape_attempts integer, neutered boolean, weight_kg decimal
 );
+ALTER TABLE animals ADD species varchar(50);
