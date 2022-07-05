a0c8f38410f0fc77f90c0e9a88148783ea3106eb
Chia Carlyle <chiacarlyle@gmail.com>
2022-07-05 11:41:12 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -3,26 +3,4 @@ INSERT INTO animals VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
 INSERT INTO animals VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
 
-INSERT INTO animals VALUES(5, 'Charmander', '2020-02-08', 0, false, -11);
-INSERT INTO animals VALUES(6, 'Plantmon', '2021-11-15', 2, true, -5.7),(7, 'Squirtle', '1993-04-02', 3, false, -12.13), (8, 'Angemon', '2005-06-12', 1, true, -45), (9, 'Boarmon', '2005-06-7', 7, true, 20.4), (10, 'Blossom', '1998-10-13', 3, true, 17), (11, 'Ditto', '2022-05-14', 4, true, 22);
 
-BEGIN;
-UPDATE animals SET species = 'unspecified';
-ROLLBACK;
-
-BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
-COMMIT;
-
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT SP1;
-UPDATE animals SET weight_kg = -1 * weight_kg;
-ROLLBACK TO SP1;
-UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
-COMMIT;
